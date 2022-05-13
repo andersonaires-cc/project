87dd4bc7902f14c0075af3d0217a3c87a6121f0d
Cassio Felipe <cassio.felipe.sarmento@gmail.com>
2022-05-13 15:51:13 -0300

    Externalização de funções no Javascript.

--- tab.js
+++ tab.js
@@ -1,39 +1,41 @@
+function adicionaCliente(e) {
+  e.preventDefault();
 
+  let toadd = {
+    matricula: document.getElementById(`inputMatricula`).value,
+    nome: document.getElementById(`inputnome`).value,
+    idade: document.getElementById(`inputidade`).value,
+    cidade: document.getElementById(`inputcidade`).value,
+    banco: document.getElementById(`inputbanco`).value,
+    poupanca: document.getElementById(`inputpoupanca`).value,
+  };
 
-function adicionaCliente(e){
-    e.preventDefault();
-    let toadd = {
-        matricula:  document.getElementById(`inputMatricula`).value,
-        nome: document.getElementById(`inputnome`).value,
-        idade: document.getElementById(`inputidade`).value,
-        cidade: document.getElementById(`inputcidade`).value,
-        banco: document.getElementById(`inputbanco`).value,
-        poupanca: document.getElementById(`inputpoupanca`).value
+  if ( hasEmptyAttributes(toadd) === true ) return;
+
+  let tr = createTRsForItem(item);
+
+  $("table:first tbody").append(tr);
+
+  if ( toadd.poupanca < 100 ) $("table:eq(1) tbody").append( $(tr).clone() );
+}
+
+function hasEmptyAttributes(item) {
+  for (i in item)
+    if (item[i] === "") {
+      alert("Preencher " + i + ".");
+      return false;
     }
+  return true;
+}
 
-    let vazio = false;
+function createTRsForItem(item) {
+  let tr = document.createElement("tr");
+  
+  for (i in item) {
+    let td = document.createElement("td");
+    td.innerHTML = item[i];
+    tr.appendChild(td);
+  }
 
-    for (i in toadd){
-        if(toadd[i] === ''){
-          alert("Preencher "+i+".");
-          vazio = true;
-          break;
-        }
-    }
-
-    if(!vazio){
-
-        let tr = document.createElement("tr")
-        for(i in toadd){
-            let td = document.createElement('td');
-            td.innerHTML = toadd[i];
-            tr.appendChild(td);
-        }
-        
-        $('table:first tbody').append(tr);
-
-        if(toadd.poupanca<100)
-            $('table:eq(1) tbody').append($(tr).clone());
-    }
- 
+  return tr;
 }
